b0524aed95b0f922a0c4eccf279f8b72b7e3926a
Marko Samoilovic <msamoilovic@gmail.com>
2018-02-14 14:18:52 +0100

    work in progress

--- external.js
+++ external.js
@@ -3,7 +3,7 @@ var rad = function (x) {
 };
 
 var getDistance = function (p1, p2) {
-    var R = 6378137; // Earth’s mean radius in meter
+    var R = 6378137;
     var dLat = rad(p2.lat - p1.lat);
     var dLong = rad(p2.lng - p1.lng);
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
@@ -11,5 +11,5 @@ var getDistance = function (p1, p2) {
         Math.sin(dLong / 2) * Math.sin(dLong / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
-    return d; // returns the distance in meter
+    return d;
 };
--- main.js
+++ main.js
@@ -1,87 +1,167 @@
 var currentLat = 0;
 var currentLng = 0;
 
+var currentSort = 0;
+var multicurrency = false;
+
 var resultsOfTheSearch = [];
+
+var resultsRepacked = [];
 
 window.onload = function () {
     var startPos;
     var geoSuccess = function (position) {
         startPos = position;
-        document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-        document.getElementById('startLon').innerHTML = startPos.coords.longitude;
         currentLat = startPos.coords.latitude;
         currentLng = startPos.coords.longitude;
-        searchForATMs();
+        initialize();
     };
 
     navigator.geolocation.getCurrentPosition(geoSuccess);
 };
 
-function searchForATMs() {
+function initialize() {
     var pyrmont = new google.maps.LatLng(currentLat, currentLng);
     console.log("poslate vrednosti", currentLat, currentLng);
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: pyrmont,
-        zoom: 15,
-        scrollwheel: false
+        zoom: 16,
+        scrollwheel: true
     });
 
-    // Specify location, radius and place types for your Places API search.
+    new google.maps.Marker({
+        map: map,
+        position: pyrmont,
+        color: "blue",
+        label: "U"
+    });
+
     var request = {
         location: pyrmont,
-        //    radius: '10000',
-        //    name:'Telenor',
         rankBy: google.maps.places.RankBy.DISTANCE,
         types: ['atm']
     };
 
-    // Create the PlaceService and send the request.
-    // Handle the callback with an anonymous function.
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, function (results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             resultsOfTheSearch = results;
-            fillTheTable(resultsOfTheSearch);
-
+            repackResults();
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
-                // If the request succeeds, draw the place location on
-                // the map as a marker, and register an event to handle a
-                // click on the marker.
                 var marker = new google.maps.Marker({
                     map: map,
-                    position: place.geometry.location
+                    position: place.geometry.location,
+                    label: i + 1 + ''
                 });
-
-                //   console.log("Rezultat broj "+ i+": ", place);
-                //   document.getElementById("primer").innerHTML += "<tr><td>"+results[i].name + "</td></tr>";
             }
         }
     });
 }
 
-function fillTheTable(results) {
-
-    var map = "<img src='https://maps.googleapis.com/maps/api/staticmap?";
-    map += "center=" + currentLat + "," + currentLng + "&size=600x300&maptype=roadmap";
-
-    document.getElementById("tableResults").innerHTML = "";
-    document.getElementById("tableResults").innerHTML += "<table><tr><th>Map</th><th>Name</th><th>Distance</th></tr>";
-    for (var i = 0; i < 10; i++) {
-        var dist = getDistance(
-            { lat: currentLat, lng: currentLng },
-            { lat: results[i].geometry.location.lat(), lng: results[i].geometry.location.lng() }
-        );
-
-        var smallMap = "<img src='https://maps.googleapis.com/maps/api/staticmap?";
-        smallMap += "center=" + results[i].geometry.location.lat() + "," + results[i].geometry.location.lng() + "&size=200x200&maptype=roadmap"
-        smallMap += "&markers=color:blue%7Clabel:S%7C" + results[i].geometry.location.lat() + "," + results[i].geometry.location.lng() + "'>";
-
-        document.getElementById("tableResults").innerHTML += "<tr><td>" + smallMap + "</td><td>" + results[i].name + "</td><td>" + dist + "</td></tr>";
-        map += "&markers=color:blue%7Clabel:S%7C" + results[i].geometry.location.lat() + "," + results[i].geometry.location.lng();
+function toggleMap() {
+    var currentState = document.getElementById("responsiveMap").style.display;
+    if (currentState == "block") {
+        document.getElementById("responsiveMap").style.display = "none";
+        document.getElementById("toggleMapBtn").innerHTML = "Show Map";
     }
-    map += "'>";
-    document.getElementById("tableResults").innerHTML += "</table>";
-    document.getElementById("tableResults").innerHTML += map;
+    else {
+        document.getElementById("responsiveMap").style.display = "block";
+        document.getElementById("toggleMapBtn").innerHTML = "Hide Map";
+    }
+}
+
+function toggleList() {
+    var currentState = document.getElementById("listDisplay").style.display;
+    if (currentState == "block") {
+        document.getElementById("listDisplay").style.display = "none";
+        document.getElementById("toggleListBtn").innerHTML = "Show List";
+    }
+    else {
+        document.getElementById("listDisplay").style.display = "block";
+        document.getElementById("toggleListBtn").innerHTML = "Hide List";
+    }
+}
+
+function multicurrencyFilter(cb) {
+    multicurrency = cb.checked;
+    generateTable(currentSort, multicurrency);
+}
+
+function changeSort() {
+    if (currentSort == 1) currentSort = 0
+    else currentSort = 1;
+    generateTable(currentSort, multicurrency);
+}
+
+function repackResults() {
+    resultsRepacked = [];
+    for (var i = 0; i < resultsOfTheSearch.length; i++) {
+
+        var mapImg = "<img src='https://maps.googleapis.com/maps/api/staticmap?";
+        mapImg += "center=" + resultsOfTheSearch[i].geometry.location.lat() + "," + resultsOfTheSearch[i].geometry.location.lng() + "&size=100x100&maptype=roadmap"
+        mapImg += "&markers=color:blue%7Clabel:" + (i + 1) + "%7C" + resultsOfTheSearch[i].geometry.location.lat() + "," + resultsOfTheSearch[i].geometry.location.lng() + "'>";
+
+        var result = {
+            mapImg: mapImg,
+            name: resultsOfTheSearch[i].name,
+            distance: Math.floor(getDistance(
+                { lat: currentLat, lng: currentLng },
+                { lat: resultsOfTheSearch[i].geometry.location.lat(), lng: resultsOfTheSearch[i].geometry.location.lng() }
+            ))
+        };
+
+        resultsRepacked.push(result);
+    }
+    generateTable(currentSort, multicurrency);
+}
+
+function generateTable(sortDirection, multicurrency) {
+
+    var tableRows = [];
+    if (multicurrency) {
+        tableRows = resultsRepacked.filter((row) => row.name.indexOf('Telenor') > -1);
+    }
+    else {
+        tableRows = resultsRepacked;
+    }
+
+    if (sortDirection == 0) {
+        tableRows = tableRows.sort((el1, el2) => parseFloat(el2.distance) - parseFloat(el1.distance));
+    }
+    else {
+        tableRows = tableRows.sort((el1, el2) => parseFloat(el1.distance) - parseFloat(el2.distance));
+    }
+
+    drawTable(tableRows);
+
+}
+
+function drawTable(tableRows) {
+    document.getElementById("resultList").innerHTML = "";
+    var table = document.getElementById("resultList");
+
+    var header = table.createTHead();
+    var hrow = header.insertRow(0);
+    var hcell1 = hrow.insertCell(0);
+    var hcell2 = hrow.insertCell(1);
+    var hcell3 = hrow.insertCell(2);
+    hcell1.innerHTML = "<b>Map</b>";
+    hcell2.innerHTML = "<b>Name</b>";
+    hcell3.innerHTML = "<button class='btn btn-link' id='distLink' onclick='changeSort()'><b>Distance</b></button>";
+
+    for (var i = 0; i < 10; i++) {
+        if (i < tableRows.length) {
+            var row = table.insertRow(i + 1);
+
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+
+            cell1.innerHTML = tableRows[i].mapImg;
+            cell2.innerHTML = tableRows[i].name;
+            cell3.innerHTML = tableRows[i].distance;
+        }
+    }
 }
